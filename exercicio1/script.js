e30a106b235c37e31cddaa6b115865dddae37c39
Alex <alexferreiraa20@gmail.com>
2022-09-01 15:25:45 -0300

    Exercicios resolvidos Alex Ferreira

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNumero = [1, 10, 8, 14]
+const arrayString = ["Olá Mundo"]
+const arrayMix = [5, "Hey-Ho", true]
+
+console.log(arrayNumero)
+console.log(arrayString)
+console.log(arrayMix)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,26 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log("arrays principais")
+const arrayNumero = [1, 10, 8, 14]
+const arrayString = ["Olá Mundo", "Tem cabaré essa noite"]
+const arrayMix = [5, "Hey-Ho", true]
+
+
+
+//quantidade de caracteres de cada array
+console.log(arrayNumero.length)
+console.log(arrayString.length)
+console.log(arrayMix.length)
+
+// primeiro,segundo e terceiro item do array respectivamente
+console.log(arrayNumero[0])
+console.log(arrayString[1])
+console.log(arrayMix[2])
+
+console.log(arrayNumero.includes(1))
+console.log(arrayMix.includes("sim"))
+
+
+
+
+
+
+
